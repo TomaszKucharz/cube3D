4cf5180a95e3eb7a47efb36c25fc96750b4649ff
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-06-22 08:50:41 +0200

    change size of cube

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const brum = document.querySelector(`.cube`);
 const expolde = document.querySelector(`.explode`);
-const color = document.querySelectorAll(`span`);
+// const color = document.querySelectorAll(`span`);
 
 const clear = function () {
   brum.classList.remove(`explode`, `explodeY`);
 };
 
 explodeCube = () => {
-  console.log(`własnie klikłem kostkem!`);
   brum.classList.toggle("explode");
   brum.classList.toggle("explodeY");
-  setTimeout(clear, 5050);
+  setTimeout(clear, 5150);
 };
 
 brum.addEventListener(`click`, explodeCube);
